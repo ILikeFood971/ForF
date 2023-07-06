7612013bde12a0cd365df162aee5889ba0666562
ILikeFood971 <123529723+ILikeFood971@users.noreply.github.com>
2023-07-06 14:54:31 -0600

    Initial Commit

---
+++ src/client/java/net/ilikefood971/ForfClient.java
@@ -0,0 +1,10 @@
+package net.ilikefood971;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ForfClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
